96eac6b5f188716c9a86eea67b4e6b43d5520de5
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-02-10 22:35:38 +0000

    foo component creato

---
+++ basic-request/src/app/foocomponent.spec.ts
@@ -0,0 +1,7 @@
+import { Foocomponent } from './foocomponent';
+
+describe('Foocomponent', () => {
+  it('should create an instance', () => {
+    expect(new Foocomponent()).toBeTruthy();
+  });
+});
